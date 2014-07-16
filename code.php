19baf388c13e02c63a6b2c3a436039c273468182
Bappi D great <aknath.707@gmail.com>
2014-07-17 03:33:17 +0600

    Initial Commit

---
+++ code.php
@@ -0,0 +1,14 @@
+<?php
+
+function my_change_profile_tab_order() {
+    global $bp;
+
+    $bp->bp_nav['settings']['position'] = 10;
+    $bp->bp_nav['activity']['position'] = 20;
+    $bp->bp_nav['friends']['position'] = 30;
+    $bp->bp_nav['groups']['position'] = 40;
+    $bp->bp_nav['blogs']['position'] = 50;
+    $bp->bp_nav['messages']['position'] = 60;
+    $bp->bp_nav['profile']['position'] = 70;
+    }
+add_action( ‘bp_setup_nav’, ‘my_change_profile_tab_order’, 999 );
